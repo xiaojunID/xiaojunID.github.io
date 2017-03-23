0c0c9f44982731705f71e2547c7ec1063ac1940f
xiaojun du <xiaojun@id.iit.edu>
2017-03-23 01:35:01 -0500

    index+about

---
+++ TEST/navgator.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+    <title>home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+   
+    <link href="https://fonts.googleapis.com/css?family=Roboto|PT+Serif|Oswald" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+    crossorigin="anonymous">
+
+    <link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+     <link rel="stylesheet" type="text/css" href="assests/css/style.css">
+    <!--<style type="text/css">
+     body {background-color:#f2f2f2;
+           font-family: 'Oswald', sans-serif;}
+    </style>-->
+        <!--=================================================-->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+</body>
+</head>
+
+<body>
+
+
+
+</html>
